73cfd4f28361b107c476f5f5188557cc182fc66a
DanielSpatariu <63121247+DanielSpatariu@users.noreply.github.com>
2020-10-04 20:02:54 +0300

    Add files via upload

---
+++ Spatariu-Gabriel-Daniel/L02/Controllers/StudentController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController
+    {
+
+        [ApiController]
+        [Route("[controller]")]
+        public class StudentControllers : ControllerBase
+        {
+            [HttpGet]
+            public IEnumerable<Student> GetStudents()
+            {
+                return Stud.getAll();
+            }
+
+            [HttpGet("{id}")]
+            public Student GetStudentsbyId([FromRoute] int id)
+            {
+                return Stud.getById(id);
+            }
+
+            [HttpPost]
+            public string CreateStudent([FromBody] Student student)
+            {
+                try
+                {
+                    Console.WriteLine(student.ToString());
+                    Stud.insert(student);
+                    return "Student created successfully";
+                }
+                catch (System.Exception error)
+                {
+                    return "Eroare: " + error.Message;
+                    throw;
+                }
+            }
+
+            [HttpPut("{id}")]
+            public string UpdateStudent([FromRoute] int id, [FromBody] Student student)
+            {
+                Student updatedStudent = Stud.update(id, student);
+                return updatedStudent.ToString();
+            }
+
+            [HttpDelete("{id}")]
+            public string DeleteStudentById([FromRoute] int id)
+            {
+                Stud.deleteById(id);
+                return "Student removed successfully";
+            }
+        }
+    }
+}
---
+++ Spatariu-Gabriel-Daniel/L02/Stud.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public static class Stud
+    {
+        private static List<Student> students = new List<Student>();
+
+        public static void insert(Student data)
+        {
+            data.Id = students.Count;
+            students.Add(data);
+        }
+
+        public static Student getById(int id)
+        {
+            return students.Find(student => student.Id == id);
+        }
+
+        public static List<Student> getAll()
+        {
+            return students;
+        }
+
+        public static void deleteById(int id)
+        {
+            students.RemoveAll(s => s.Id == id);
+        }
+
+        public static Student update(int id, Student student)
+        {
+            int index = students.FindIndex(s => s.Id == id);
+            students[index].FirstName = student.FirstName;
+            students[index].LastName = student.LastName;
+            students[index].Faculty = student.Faculty;
+            students[index].YOS = student.YOS;
+            return students[index];
+        }
+    }
+
+}
